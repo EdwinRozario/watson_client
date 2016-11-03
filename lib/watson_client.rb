e00d92210088607b588d5c9b34d08f6b0ac6cb67
Edwin Rozario <edwin@boost.co.nz>
2016-11-04 10:27:04 +1300

    Added Service module

--- lib/watson_client.rb
+++ lib/watson_client.rb
@@ -1,4 +1,11 @@
+# frozen_string_literal: true
+require 'json'
+require 'openssl'
+require 'open-uri'
+require 'rest-client'
+
 require "watson_client/version"
+require "watson_client/service"
 
 module WatsonClient
   # Your code goes here...
---
+++ lib/watson_client/service.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module WatsonClient
+  # All Watson services defined here
+  class Service
+    attr_reader :api_docs, :gateways, :doc_urls, :all
+    def initialise
+      @api_docs = {
+        gateway: 'https://gateway.watsonplatform.net',
+        gateway_a: 'https://gateway-a.watsonplatform.net',
+        doc_base1: 'https://watson-api-explorer.mybluemix.net/',
+        doc_base2: 'http://www.ibm.com/watson/developercloud/doc/',
+        ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE
+      }
+
+      update_api_docs
+      
+      @gateways = fetch_gateways
+      @doc_urls = fetch_doc_urls
+    end
+
+    def fetch_gateways
+      { gateway: @api_docs.delete(:gateway),
+        gateway_a: @api_docs.delete(:gateway_a) }
+    end
+
+    def fetch_doc_urls
+      { doc_base1: api_docs.delete(:doc_base1),
+        doc_base2: api_docs.delete(:doc_base2) }
+    end
+
+    def update_api_docs
+      docs = JSON.parse(ENV['WATSON_API_DOCS'] || '{}')
+      return if docs.empty?
+
+      docs.each_pair do |key, value|
+        @api_docs[key.to_sym] = value
+      end
+    end
+  end
+end
+
+
+  # Options  = api_docs
+  # Services = JSON.parse(ENV['VCAP_SERVICES'] || '{}')
+  # DefaultParams = {:user=>'username', :password=>'password'}
+  # AvailableAPIs = []
